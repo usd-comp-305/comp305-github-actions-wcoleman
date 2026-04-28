b0dec3f6dc8dad652efe565edcb4b668959381aa
Jake Coleman <wcoleman@sandiego.edu>
2026-04-27 19:25:26 -0700

    style changes to Main class

--- src/main/java/edu/sandiego/comp305/Main.java
+++ src/main/java/edu/sandiego/comp305/Main.java
@@ -1,8 +1,27 @@
 package edu.sandiego.comp305;
 
-public class Main {
+/**
+ * Main application class.
+ */
+public final class Main {
 
-    public static void main(String[] args) {
+    /**
+     * Private constructor
+     */
+    private Main() {
 
+    }
+
+    /**
+     * Main entry point.
+     * @param args Command line arguments
+     */
+    public static void main(final String[] args) {
+        System.out.printf("Hello and welcome!%n");
+
+        final int max = 5;
+        for (int i = 1; i <= max; i++) {
+            System.out.println("i = " + i);
+        }
     }
 }
